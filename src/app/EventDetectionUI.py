7bf8e9f6db9abe80af8ed4777d1227487346da7d
AndreeaDraghici <draghici108@gmail.com>
2023-08-20 20:37:04 +0300

    extend functionality to have the possibility to select the object detected you want

--- src/app/EventDetectionUI.py
+++ src/app/EventDetectionUI.py
@@ -39,6 +39,9 @@ class EventDetectionUI :
             self.label_text = tk.StringVar()
             self.label = tk.Label(root, textvariable=self.label_text)
             self.label.pack()
+
+            self.detected_objects_buttons = []  # List to store buttons
+
         except Exception as e :
             self.logger.error("An error occurred during UI initialization: %s", str(e))
 
@@ -75,12 +78,23 @@ class EventDetectionUI :
 
                     self.db_manager.insert_detected_objects(detected_objects)
                     self.display_image()
-                    self.update_detected_objects(unique_detected_objects)
-                    self.root.after(5, self.update_detected_objects, unique_detected_objects)
+
+                    # Clear existing buttons
+                    for button in self.detected_objects_buttons :
+                        button.destroy()
+                    self.detected_objects_buttons.clear()
+
+                    # Update the detected objects with buttons
+                    for obj_label in unique_detected_objects :
+                        button = tk.Button(self.root, text=obj_label,
+                                           command=lambda label=obj_label : self.display_selected_object(label))
+                        button.pack()
+                        self.detected_objects_buttons.append(button)
+
+                    self.label_text.set("Detected Objects: " + ", ".join(unique_detected_objects))
         except Exception as e :
             self.logger.error("An error occurred during object detection: %s", str(e))
 
-    ''' 
     def display_selected_object(self, obj_label) :
         if self.image is not None and self.image_path :
             detected_objects = self.backend.detect_objects(self.image_path)
@@ -102,7 +116,7 @@ class EventDetectionUI :
                     # Desenează un chenar în jurul obiectului și afișează eticheta
                     cv2.rectangle(selected_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
                     label_position = (x, y + h + 20)  # Ajustați poziția etichetei
-                    cv2.putText(selected_img, obj_label, label_position, cv2.FONT_HERSHEY_SIMPLEX,0.7, (0, 255, 0), 2)
+                    cv2.putText(selected_img, obj_label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
                     img_pil = Image.fromarray(np.uint8(selected_img))
                     img_tk = ImageTk.PhotoImage(image=img_pil)
@@ -110,7 +124,6 @@ class EventDetectionUI :
                     self.canvas.create_image(0, 0, anchor=tk.NW, image=img_tk)
                     self.canvas.image = img_tk
                     break
-    '''
 
     def display_image(self) :
         try :
