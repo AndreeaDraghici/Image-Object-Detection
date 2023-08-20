aef36ed9177fb4195e6f445f23a3f6ddf2137b34
AndreeaDraghici <draghici108@gmail.com>
2023-08-20 22:12:51 +0300

    added explanation comments

--- src/app/EventDetection.py
+++ src/app/EventDetection.py
@@ -9,31 +9,53 @@ from src.LoadLoggingConfig import load_logging_config
 class EventDetection :
 
     def __init__(self) :
+        # Load the logging configuration
         load_logging_config()
+        # Get the logger for the 'development' logger
         self.logger = logging.getLogger('development')
 
         try :
             # Initialize the YOLO detector
             self.net = cv2.dnn.readNet('../model/yolov3.cfg', '../model/yolov3.weights')
+
         except cv2.error as e :
+            # Log an error if YOLO initialization fails
             self.logger.error("Failed to initialize YOLO detector: %s", str(e))
 
+        # Initialize an empty list to store class names from coco.names file
         self.classes = []
+
         try :
+            # Read class names from coco.names file
             with open('../model/coco.names', 'r') as f :
                 self.classes = f.read().splitlines()
+
         except IOError as e :
+            # Log an error if reading coco.names file fails
             self.logger.error("Failed to read coco.names file: %s", str(e))
 
+        # Initialize an empty list to store detected objects
         self.detected_objects = []
 
     def detect_objects(self, image_path) :
+        """
+            Detects objects in an image using YOLO model.
+
+            Args:
+                image_path (str): The path to the image file.
+
+            Returns:
+                list: List of detected objects, each represented by a tuple of class label and bounding box coordinates.
+        """
         try :
+            # Load the image and preprocess it for YOLO
             image = cv2.imread(image_path)
             blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
             self.net.setInput(blob)
             outs = self.net.forward(self.get_output_layers())
+
         except (cv2.error, IOError) as e :
+            # Log an error if image processing or forward pass fails
             self.logger.error("Failed to process image or forward pass: %s", str(e))
             return []
 
@@ -42,6 +64,7 @@ class EventDetection :
         boxes = []
 
         try :
+            # Extract detected objects' information
             for out in outs :
                 for detection in out :
                     scores = detection[5 :]
@@ -57,11 +80,14 @@ class EventDetection :
                         class_ids.append(class_id)
                         confidences.append(float(confidence))
                         boxes.append([x, y, w, h])
+
         except IndexError as e :
+            # Log an error if processing detections fails
             self.logger.error("Failed to process detections: %s", str(e))
             return []
 
         try :
+            # Apply Non-Maximum Suppression to filter out redundant detections
             indexes = cv2.dnn.NMSBoxes(boxes, confidences, score_threshold=0.5, nms_threshold=0.4)
             detected_objects = []
             for i in range(len(boxes)) :
@@ -69,16 +95,27 @@ class EventDetection :
                     label = str(self.classes[class_ids[i]])
                     detected_objects.append((label, boxes[i]))
             return detected_objects
+
         except cv2.error as e :
+            # Log an error if NMS or processing detected objects fails
             self.logger.error("Failed to perform NMS or process detected objects: %s", str(e))
             return []
 
     def get_output_layers(self) :
+        """
+            Retrieves the names of the output layers of the YOLO model.
+
+            Returns:
+                list: List of output layer names.
+        """
         try :
+            # Get output layers' names for YOLO
             layers_names = self.net.getLayerNames()
             unconnected_layers = self.net.getUnconnectedOutLayers()
             output_layers = [layers_names[layer - 1] for layer in unconnected_layers]
             return output_layers
+
         except (cv2.error, IndexError) as e :
+            # Log an error if getting output layers fails
             self.logger.error("Failed to get output layers: %s", str(e))
             return []
--- src/db/DatabaseManager.py
+++ src/db/DatabaseManager.py
@@ -7,12 +7,15 @@ from src.LoadLoggingConfig import load_logging_config
 class DatabaseManager :
     def __init__(self) :
         load_logging_config()
-        self.logger = logging.getLogger('EventDetectionUI')
+        # Initialize logger using the specified name
+        self.logger = logging.getLogger('development')
 
         try :
+            # Establish a connection to the SQLite database
             self.conn = sqlite3.connect('../db/detected_objects.db')
             self.cursor = self.conn.cursor()
 
+            # Create a table if it doesn't exist to store detected objects
             self.cursor.execute('''
                 CREATE TABLE IF NOT EXISTS detected_objects (
                     id INTEGER PRIMARY KEY,
@@ -23,45 +26,73 @@ class DatabaseManager :
                     h INTEGER
                 )
             ''')
-
+            # Commit the changes to the database
             self.conn.commit()
         except Exception as e :
+            # Log an error if any exception occurs during database initialization
             self.logger.error("An error occurred during database initialization:", str(e))
 
     def insert_detected_objects(self, detected_objects) :
+        """
+                Insert detected objects and their coordinates into the database.
+
+                :param detected_objects: List of tuples containing object type and coordinates
+        """
         try :
+            # Iterate through detected objects and insert them into the database
             for obj_label, coords in detected_objects :
                 x, y, w, h = coords
                 self.cursor.execute('INSERT INTO detected_objects (object_type, x, y, w, h) VALUES (?, ?, ?, ?, ?)',
                                     (obj_label, x, y, w, h))
+            # Commit the changes to the database
             self.conn.commit()
         except Exception as e :
+            # Log an error if any exception occurs during insertion
             self.logger.error("An error occurred during inserting detected objects:", str(e))
 
     def close_connection(self) :
         try :
+            # Close the database connection
             self.conn.close()
         except Exception as e :
+            # Log an error if any exception occurs during connection closing
             self.logger.error("An error occurred during closing connection:", str(e))
 
     def get_detected_objects(self) :
+        """
+              Retrieve a list of detected object types from the database.
+
+              :return: List of detected object types
+        """
         try :
+            # Execute a query to retrieve the object types of detected objects
             self.cursor.execute("SELECT object_type FROM detected_objects")
             detected_objects = self.cursor.fetchall()
+            # Return a list of detected object types
             return [row[0] for row in detected_objects]
         except Exception as e :
+            # Log an error if any exception occurs during retrieval
             self.logger.error("An error occurred during retrieving detected objects:", str(e))
             return []
 
     def get_object_coordinates(self, obj_label) :
+        """
+            Retrieve the coordinates of a specific object type from the database.
+
+            :param obj_label: Object type
+            :return: Coordinates (x, y, w, h) of the specified object type, or None if not found
+        """
         try :
+            # Execute a query to retrieve the coordinates of a specific object type
             self.cursor.execute("SELECT x, y, w, h FROM detected_objects WHERE object_type = ?", (obj_label,))
             result = self.cursor.fetchone()
             if result :
+                # If the object exists, extract and return its coordinates
                 x, y, w, h = result
                 return x, y, w, h
             else :
                 return None
         except Exception as e :
+            # Log an error if any exception occurs during retrieval
             self.logger.error("An error occurred during retrieving object coordinates:", str(e))
             return None
